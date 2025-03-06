23e7be18914c6556b1ff60cabc8dffa2eb2160e9
Juan <jj.monte2002@gmail.com>
2025-03-05 20:18:32 -0500

    Fifth commit

--- module/Personas/src/Form/PersonaForm.php
+++ module/Personas/src/Form/PersonaForm.php
@@ -5,31 +5,76 @@ namespace Persona\Form;
 use Laminas\Form\Element\Hidden;
 use Laminas\Form\Element\Submit;
 use Laminas\Form\Element\Text;
+use Laminas\Form\Element\Tel;
+use Laminas\Form\Element\Select;
 use Laminas\Form\Form;
 
-class PersonaForm extends Form
-{
-    public function __construct($name = null)
-    {
+class PersonaForm extends Form {
+
+    public function __construct($departamentos = [],$municipios = []) {
         // We will ignore the name provided to the constructor
-        parent::__construct('album');
+        parent::__construct('persona');
 
         $this->add([
-            'name' => 'id',
-            'type' => Hidden::class,
+            'name' => 'idPersona',
+            'type' => Text::class,
         ]);
         $this->add([
-            'name' => 'title',
+            'name' => 'nombres',
             'type' => Text::class,
             'options' => [
-                'label' => 'Title',
+                'label' => 'Nombres',
             ],
         ]);
         $this->add([
-            'name' => 'artist',
+            'name' => 'apellidos',
             'type' => Text::class,
             'options' => [
-                'label' => 'Artist',
+                'label' => 'Apellidos',
+            ],
+        ]);
+        $this->add([
+            'name' => 'identificacion',
+            'type' => Text::class,
+            'options' => [
+                'label' => 'Identificacion',
+            ],
+            'attributes' => [
+                'onchange' => 'verificarIdentificacion()',
+                'id' => 'identificacion',
+            ],
+        ]);
+        $this->add([
+            'name' => 'celular',
+            'type' => Tel::class,
+            'options' => [
+                'label' => 'Celuar',
+            ],
+        ]);
+        $this->add([
+            'name' => 'idMunicipio',
+            'type' => Select::class,
+            'options' => [
+                'label' => 'Municipio',
+                'empty_option' => 'Seleccione...',
+                'value_options' => $municipios,
+                'disable_inarray_validator'=>true
+            ],
+            'attributes' => [
+                'id' => 'municipio',
+            ],
+        ]);
+        
+        $this->add([
+            'name' => 'departamento',
+            'type' => Select::class,
+            'options' => [
+                'label' => 'Departamento',
+                'empty_option' => 'Seleccione...',
+                'value_options' => $departamentos,
+            ],'attributes' => [
+                'onchange'=>'getMunicipios()',
+                'id' => 'departamento',
             ],
         ]);
         $this->add([
@@ -37,9 +82,8 @@ class PersonaForm extends Form
             'type' => Submit::class,
             'attributes' => [
                 'value' => 'Go',
-                'id'    => 'submitbutton',
+                'id' => 'submitbutton',
             ],
         ]);
     }
 }
-
